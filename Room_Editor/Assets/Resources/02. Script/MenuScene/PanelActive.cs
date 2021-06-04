bc3d537470f6f066df1d39ba5a694d6e9c2ce236
LimChaeJune <cownsgg@outlook.com>
2021-06-04 18:01:33 +0900

    update

--- Room_Editor/Assets/Resources/02. Script/MenuScene/PanelActive.cs
+++ Room_Editor/Assets/Resources/02. Script/MenuScene/PanelActive.cs
@@ -12,22 +12,29 @@ public class PanelActive : MonoBehaviour
 
     public float SmoothTime = 0.5f;
 
+    private CanvasGroup MyCanvasGrp;
+
     public bool IsShow = false;
     
     public void Start()
     {        
-        ThisRect = GetComponent<RectTransform>();        
+        ThisRect = GetComponent<RectTransform>();
+        MyCanvasGrp = GetComponent<CanvasGroup>();
     }
 
     private void Update()
     {
         if (IsShow)
         {
-            ThisRect.localPosition = Vector3.SmoothDamp(ThisRect.localPosition, new Vector3(0, 0, 0),  ref buttonVelocity, SmoothTime);
+            MyCanvasGrp.alpha = Mathf.Lerp(MyCanvasGrp.alpha, 1, Time.deltaTime);
+            //ThisRect.localScale = Vector3.Lerp(transform.localScale, new Vector3(1, 1, 1), SmoothTime);
+            //ThisRect.localPosition = Vector3.SmoothDamp(ThisRect.localPosition, new Vector3(0, 0, 0),  ref buttonVelocity, SmoothTime);
         }
         else
         {
-            ThisRect.localPosition = Vector3.SmoothDamp(ThisRect.localPosition, ShowPos , ref buttonVelocity, SmoothTime);
+            MyCanvasGrp.alpha = Mathf.Lerp(MyCanvasGrp.alpha, 0, Time.deltaTime);
+            //ThisRect.localScale = Vector3.Lerp(transform.localScale, new Vector3(0, 0, 0), SmoothTime);
+            //ThisRect.localPosition = Vector3.SmoothDamp(ThisRect.localPosition, ShowPos , ref buttonVelocity, SmoothTime);
         }
     }   
 }
